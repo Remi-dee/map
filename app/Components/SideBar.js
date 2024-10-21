080e643cd2c4a1616024816d00a7acb17302a134
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-10-21 14:05:06 +0000

    Monday, October 21, 2024 at 3:05 PM West Africa Standard Time

--- app/Components/SideBar.js
+++ app/Components/SideBar.js
@@ -7,9 +7,12 @@ import {
   UserIcon,
   CogIcon,
 } from "@heroicons/react/outline"; // Use any icons
+import { BiUserVoice } from "react-icons/bi";
 import DoubleArrow from "@/app/assets/doubleArrowRight.png";
 import Image from "next/image";
 import User from "@/app/assets/Profile.png";
+import Message from "@/app/assets/message.png";
+import { PiChatsCircle } from "react-icons/pi";
 const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
@@ -45,10 +48,32 @@ const Sidebar = () => {
               )}
             </li>
             <li className="flex items-center p-4 hover:bg-gray-200">
+              <BiUserVoice className="h-6 w-6 text-gray-500" />
+              {!isCollapsed && (
+                <span className="ml-4  text-[14px] font-normal leading-[20px] text-left">
+                  Events
+                </span>
+              )}
+            </li>
+            <li className="flex items-center p-4 hover:bg-gray-200">
               <BellIcon className="h-6 w-6 text-gray-500" />
               {!isCollapsed && (
                 <span className="ml-4 text-[14px] font-normal leading-[20px] text-left">
                   Notifications
+                </span>
+              )}
+            </li>
+            <li className="flex items-center p-4 hover:bg-gray-200">
+              <Image
+                src={Message}
+                width={20}
+                height={20}
+                className={`rotate-180 ${isCollapsed && " rotate-180"} `}
+              />
+              <PiChatsCircle className="h-6 w-6 text-gray-500" />
+              {!isCollapsed && (
+                <span className="ml-4 text-[14px] font-normal leading-[20px] text-left">
+                  Messages
                 </span>
               )}
             </li>
