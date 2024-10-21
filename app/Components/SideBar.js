a84fa8a75d80227ff49727353909ce591c6e82cd
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-10-21 15:13:25 +0000

    Monday, October 21, 2024 at 4:13 PM West Africa Standard Time

--- app/Components/SideBar.js
+++ app/Components/SideBar.js
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 import {
   HomeIcon,
   CalendarIcon,
@@ -41,67 +42,67 @@ const Sidebar = () => {
     <div
       className={`hidden lg:flex  ${
         isCollapsed ? "w-16" : ""
-      } h-screen transition-width dark:bg-gray-800 dark:border-gray-700 duration-300 p-[8px] bg-white border-r border-[#F1F5F9]`}
+      } h-screen transition-width dark:bg-[#484554] dark:border-gray-700 duration-300 p-[8px] bg-white border-r border-[#F1F5F9]`}
     >
       {/* Sidebar content */}
       <div
         className={`flex flex-col justify-between ${
-          isCollapsed ? "w-16" : "w-[238px]"
+          isCollapsed ? "w-14" : "w-[238px]"
         }`}
       >
         <div>
           <ul className="mt-4 ">
             {" "}
-            <li className="flex items-center p-4  hover:bg-gray-200 dark:hover:bg-gray-600">
-              <HomeIcon className="h-6 w-6 text-gray-500 dark:text-gray-300" />
+            <li className="flex items-center p-4  hover:bg-[#FCF7FF] dark:hover:bg-[#8576FF]">
+              <HomeIcon className="h-6 w-6 text-gray-500 dark:text-[#FFFFFF]" />
               {!isCollapsed && (
-                <span className="ml-4 text-[14px] font-normal leading-[20px] text-left dark:text-gray-300">
+                <span className="ml-4 text-[14px] font-normal leading-[20px] text-left dark:text-[#FFFFFF]">
                   Home
                 </span>
               )}
             </li>
-            <li className="flex items-center p-4 hover:bg-gray-200">
-              <CalendarIcon className="h-6 w-6 text-gray-500" />
+            <li className="flex items-center p-4  hover:bg-[#FCF7FF] dark:hover:bg-[#8576FF]">
+              <CalendarIcon className="h-6 w-6 text-gray-500 dark:text-[#FFFFFF]" />
               {!isCollapsed && (
-                <span className="ml-4  text-[14px] font-normal leading-[20px] text-left">
+                <span className="ml-4  text-[14px] font-normal leading-[20px] text-left dark:text-[#FFFFFF]">
                   Events
                 </span>
               )}
             </li>
-            <li className="flex items-center p-4 hover:bg-gray-200">
-              <BiUserVoice className="h-6 w-6 text-gray-500" />
+            <li className="flex items-center p-4 hover:bg-[#FCF7FF] dark:hover:bg-[#8576FF] dark:text-[#FFFFFF] ">
+              <BiUserVoice className="h-6 w-6 text-gray-500 dark:text-[#FFFFFF]" />
               {!isCollapsed && (
-                <span className="ml-4  text-[14px] font-normal leading-[20px] text-left">
-                  Events
+                <span className="ml-4  text-[14px] font-normal leading-[20px] text-left dark:text-[#FFFFFF]">
+                  Speakers
                 </span>
               )}
             </li>
-            <li className="flex items-center p-4 hover:bg-gray-200">
-              <BellIcon className="h-6 w-6 text-gray-500" />
+            <li className="flex items-center p-4 hover:bg-[#FCF7FF] dark:hover:bg-[#8576FF]">
+              <BellIcon className="h-6 w-6 text-gray-500 dark:text-[#FFFFFF]" />
               {!isCollapsed && (
-                <span className="ml-4 text-[14px] font-normal leading-[20px] text-left">
+                <span className="ml-4 text-[14px] font-normal leading-[20px] text-left dark:text-[#FFFFFF]">
                   Notifications
                 </span>
               )}
             </li>
-            <li className="flex items-center p-4 hover:bg-gray-200">
-              <PiChatsCircle className="h-6 w-6 text-gray-500" />
+            <li className="flex items-center p-4 hover:bg-[#FCF7FF] dark:hover:bg-[#8576FF] dark:text-[#FFFFFF]">
+              <PiChatsCircle className="h-6 w-6 text-gray-500 dark:text-[#FFFFFF]" />
               {!isCollapsed && (
                 <span className="ml-4 text-[14px] font-normal leading-[20px] text-left">
                   Messages
                 </span>
               )}
             </li>
-            <li className="flex items-center p-4 hover:bg-gray-200">
-              <UserIcon className="h-6 w-6 text-gray-500" />
+            <li className="flex items-center p-4 hover:bg-[#FCF7FF] dark:hover:bg-[#8576FF] dark:text-[#FFFFFF]">
+              <UserIcon className="h-6 w-6 text-gray-500 dark:text-[#FFFFFF]" />
               {!isCollapsed && (
                 <span className="ml-4  text-[14px] font-normal leading-[20px] text-left">
                   Profile
                 </span>
               )}
             </li>
-            <li className="flex items-center p-4 hover:bg-gray-200">
-              <CogIcon className="h-6 w-6 text-gray-500" />
+            <li className="flex items-center p-4 hover:bg-[#FCF7FF] dark:hover:bg-[#8576FF] dark:text-[#FFFFFF]">
+              <CogIcon className="h-6 w-6 text-gray-500 dark:text-[#FFFFFF]" />
               {!isCollapsed && (
                 <span className="ml-4  text-[14px] font-normal leading-[20px] text-left">
                   Settings
@@ -109,20 +110,19 @@ const Sidebar = () => {
               )}
             </li>
             <button
-              className="p-4 flex items-center"
+              className="p-4 flex items-center hover:bg-[#FCF7FF] dark:hover:bg-[#8576FF] dark:text-[#FFFFFF] w-full"
               onClick={() => setIsCollapsed(!isCollapsed)}
             >
               {/* Collapse/Expand icon */}
               <span
                 className={`${
                   isCollapsed ? "rotate-180" : ""
-                }flex items-center ml-1.5 transition-transform`}
+                }flex items-center  transition-transform`}
               >
-                <Image
-                  src={DoubleArrow}
-                  width={13}
-                  height={13}
-                  className={`rotate-180 ${isCollapsed && " rotate-180"} `}
+                <MdKeyboardDoubleArrowLeft
+                  className={` ${
+                    isCollapsed && " rotate-180"
+                  } h-6 w-6 text-gray-500 dark:text-[#FFFFFF]`}
                 />
                 <span
                   className={`${
@@ -133,8 +133,8 @@ const Sidebar = () => {
                 </span>
               </span>
             </button>
-            <div className="p-4">
-              <label className="flex items-center cursor-pointer">
+            <div className=" w-full">
+              <label className="flex items-center p-4 hover:bg-[#FCF7FF] dark:hover:bg-[#8576FF] cursor-pointer dark:text-[#FFFFFF]">
                 <input
                   type="checkbox"
                   checked={isDarkMode}
@@ -143,12 +143,16 @@ const Sidebar = () => {
                 />
 
                 <span className="toggle-thumb block w-4 h-4 bg-white dark:bg-gray-400 rounded-full shadow transform transition-transform duration-300" />
-                <span className="mr-3 text-gray-500 dark:text-gray-300">
+                <span
+                  className={`${
+                    isCollapsed && "hidden rotate-180"
+                  } ml-4 text-[14px] font-normal leading-[20px] text-left`}
+                >
                   Dark mode
                 </span>
               </label>
             </div>
-            <div className="p-4">
+            <div className="p-4 dark:text-[#FFFFFF] hover:bg-[#FCF7FF] dark:hover:bg-[#8576FF]">
               <div className="flex items-center space-x-2">
                 <Image
                   src={User}
