bb7112d0565517544712871be3d9d178a4bb415e
Remidee <remi.aluko@expatswap.com>
2024-12-22 23:48:15 +0000

    Monday, 23 December 2024 at 00:48 West Africa Standard Time

--- app/Components/RequestForQuote.js
+++ app/Components/RequestForQuote.js
@@ -1,36 +1,34 @@
-// RequestForQuote.js
-import { useDispatch, useSelector } from "react-redux";
-import {
-  addItem,
-  removeItem,
-  updateItem,
-  updateQuoteDetails,
-} from "../redux/feature/quoteSlice";
 import { useState } from "react";
 import AddItemsSection from "./AddItem";
 import TermsAndAttachments from "./TermsAndAttachment";
-import ReviewComponent from "./Review";
 
 const RequestForQuote = () => {
-  const dispatch = useDispatch();
-  const { items, quoteDetails } = useSelector((state) => state.quote);
+  const [currentScreen, setCurrentScreen] = useState("RequestForQuote");
+  const [quoteDetails, setQuoteDetails] = useState({
+    rfqNo: "RFQ-01234",
+    title: "Request for Equipments",
+    department: "Maternity",
+    expectedDeliveryDate: "",
+  });
 
-  const handleAddItem = () => {
-    dispatch(addItem());
-  };
-
-  const handleRemoveItem = (index) => {
-    dispatch(removeItem(index));
-  };
-
-  const handleInputChange = (e, index, field) => {
-    dispatch(updateItem({ index, field, value: e.target.value }));
-  };
-
-  const handleQuoteChange = (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
-    dispatch(updateQuoteDetails({ name, value }));
+    setQuoteDetails({ ...quoteDetails, [name]: value });
   };
+
+  const handleContinue = () => {
+    setCurrentScreen("TermsAndAttachments");
+  };
+
+  const handleBack = () => {
+    setCurrentScreen("RequestForQuote");
+  };
+
+  if (currentScreen === "TermsAndAttachments") {
+    return (
+      <TermsAndAttachments onBack={handleBack} quoteDetails={quoteDetails} />
+    );
+  }
 
   return (
     <div className="p-6">
@@ -46,9 +44,9 @@ const RequestForQuote = () => {
           <input
             type="text"
             name="rfqNo"
-            value={quoteDetails.rfqNo || ""}
-            onChange={handleQuoteChange}
-            className="mt-1 text-[14px] font-normal text-[#98A2B3] bg-[#F0F2F5] px-[12px] py-[10px] border-[#D0D5DD] block w-full  rounded-md shadow-sm"
+            value={quoteDetails.rfqNo}
+            onChange={handleInputChange}
+            className="mt-1 text-[14px] font-normal text-[#98A2B3] bg-[#F0F2F5] px-[12px] py-[10px] border-[#D0D5DD] block w-full rounded-md shadow-sm"
           />
         </div>
         <div>
@@ -58,9 +56,9 @@ const RequestForQuote = () => {
           <input
             type="text"
             name="title"
-            value={quoteDetails.title || ""}
-            onChange={handleQuoteChange}
-            className="mt-1  text-[14px] font-normal text-[#98A2B3] block w-full bg-[#F0F2F5] px-[12px] py-[10px] border-[#D0D5DD] rounded-md shadow-sm"
+            value={quoteDetails.title}
+            onChange={handleInputChange}
+            className="mt-1 text-[14px] font-normal text-[#98A2B3] block w-full bg-[#F0F2F5] px-[12px] py-[10px] border-[#D0D5DD] rounded-md shadow-sm"
           />
         </div>
         <div>
@@ -70,9 +68,9 @@ const RequestForQuote = () => {
           <input
             type="text"
             name="department"
-            value={quoteDetails.department || ""}
-            onChange={handleQuoteChange}
-            className="mt-1  text-[14px] font-normal text-[#98A2B3] block w-full bg-[#F0F2F5] px-[12px] py-[10px] border-[#D0D5DD] rounded-md shadow-sm"
+            value={quoteDetails.department}
+            onChange={handleInputChange}
+            className="mt-1 text-[14px] font-normal text-[#98A2B3] block w-full bg-[#F0F2F5] px-[12px] py-[10px] border-[#D0D5DD] rounded-md shadow-sm"
           />
         </div>
         <div>
@@ -82,9 +80,9 @@ const RequestForQuote = () => {
           <input
             type="date"
             name="expectedDeliveryDate"
-            value={quoteDetails.expectedDeliveryDate || ""}
-            onChange={handleQuoteChange}
-            className="mt-1  text-[14px] font-normal text-[#98A2B3] block w-full bg-[#F0F2F5] px-[12px] py-[10px] border-[#D0D5DD]  rounded-md shadow-sm"
+            value={quoteDetails.expectedDeliveryDate}
+            onChange={handleInputChange}
+            className="mt-1 text-[14px] font-normal text-[#98A2B3] block w-full bg-[#F0F2F5] px-[12px] py-[10px] border-[#D0D5DD] rounded-md shadow-sm"
           />
           <h7 className="text-xs font-[500px] text-[#667185]">
             Calculated based on lead time and issue date
@@ -92,76 +90,6 @@ const RequestForQuote = () => {
         </div>
       </div>
       <AddItemsSection />
-      <TermsAndAttachments />
-      <ReviewComponent />
-      {/* <h2 className="text-lg font-semibold mb-4">Add Items</h2>
-      <table className="w-full mb-4 border-collapse">
-        <thead>
-          <tr>
-            <th className="border p-2">Items</th>
-            <th className="border p-2">Variant</th>
-            <th className="border p-2">Quantity</th>
-            <th className="border p-2">Price</th>
-            <th className="border p-2">Expected Delivery Date</th>
-            <th className="border p-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item, index) => (
-            <tr key={index}>
-              <td className="border p-2">
-                <input
-                  type="text"
-                  value={item.name}
-                  onChange={(e) => handleInputChange(e, index, "name")}
-                  className="w-full border-gray-300 rounded-md shadow-sm"
-                />
-              </td>
-              <td className="border p-2">
-                <input
-                  type="text"
-                  value={item.variant}
-                  onChange={(e) => handleInputChange(e, index, "variant")}
-                  className="w-full border-gray-300 rounded-md shadow-sm"
-                />
-              </td>
-              <td className="border p-2">
-                <input
-                  type="number"
-                  value={item.quantity}
-                  onChange={(e) => handleInputChange(e, index, "quantity")}
-                  className="w-full border-gray-300 rounded-md shadow-sm"
-                />
-              </td>
-              <td className="border p-2">
-                <input
-                  type="number"
-                  value={item.price}
-                  onChange={(e) => handleInputChange(e, index, "price")}
-                  className="w-full border-gray-300 rounded-md shadow-sm"
-                />
-              </td>
-              <td className="border p-2">
-                <input
-                  type="date"
-                  value={item.deliveryDate}
-                  onChange={(e) => handleInputChange(e, index, "deliveryDate")}
-                  className="w-full border-gray-300 rounded-md shadow-sm"
-                />
-              </td>
-              <td className="border p-2">
-                <button
-                  onClick={() => handleRemoveItem(index)}
-                  className="text-red-500"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table> */}
-
       <div className="flex justify-end mt-6">
         <button className="bg-gray-300 text-black px-4 py-2 rounded-md mr-2">
           Cancel
@@ -169,7 +97,10 @@ const RequestForQuote = () => {
         <button className="bg-gray-500 text-white px-4 py-2 rounded-md mr-2">
           Save as Draft
         </button>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          onClick={handleContinue}
+        >
           Continue
         </button>
       </div>
